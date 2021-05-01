537215944f5058e5db24c7cc95c05111f2c017ab
shadrack.adwera <adweshshaddie@gmail.com>
2021-05-01 16:23:25 +0300

    tap into app context using useContext hook

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+  const { isLoggedIn, logout } = useContext(AuthContext);
+  
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={logout}>Logout</button>
           </li>
         )}
       </ul>
